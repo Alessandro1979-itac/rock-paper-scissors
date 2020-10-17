335eb0e3c9bb28576771211d11e958990f2c276e
Alessandro <sandromuniz27@hotmail.com>
2020-10-17 13:03:22 -0400

    feat: adds rules page

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,16 @@ const routes = [
 		component: () => import(/* webpackChunkName: "Home" */ '../pages/Home.vue'),
 	},
 	{
+		path: '/play',
+		name: 'Game',
+		component: () => import(/* webpackChunkName: "Game" */ '../pages/Game.vue'),
+	},
+	{
+		path: '/rules',
+		name: 'Rules',
+		component: () => import(/* webpackChunkName: "Rules" */ '../pages/Rules.vue'),
+	},
+	{
 		path: '*',
 		name: 'notFound',
 		redirect: '/',

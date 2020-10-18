5544a72798cf97fa0adc3587314137b176a62f96
Alessandro <sandromuniz27@hotmail.com>
2020-10-17 21:46:27 -0400

    feat: adds rules page

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,23 @@
 		@mousemove="redefinePositions"
 		@mouseleave="resetPositions"
 	>
-		<div id="nav" />
-		<RouterView />
+		<Header />
+		<div class="view" >
+			<RouterView />
+		</div>
 	</div>
 </template>
 
 <script>
+	import Header from '@/components/Header.vue';
+
 	import { bgColorFrom, bgColorTo } from './styles/variables.scss';
+
 	export default {
 		name: 'App',
+		components: {
+			Header,
+		},
 		data() {
 			return {
 				gradientX: '50%',
@@ -42,10 +50,23 @@
 
 <style lang="scss">
 	@import './styles/reset';
+
 	#app {
 		display: flex;
 		flex-direction: column;
 		height: 100%;
+		min-height: 100vh;
 		font-family: 'Barlow Semi Condensed', Helvetica, Arial, sans-serif;
+
+		.view {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex: 1;
+			width: 100%;
+			max-width: 800px;
+			margin: 0 auto;
+			padding: rfs(30px);
+		}
 	}
 </style>

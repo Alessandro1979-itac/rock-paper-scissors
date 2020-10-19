dca44acca09a77050d86f231b7ee6d96cb8897b8
Alessandro <sandromuniz27@hotmail.com>
2020-10-19 14:05:28 -0400

    feat: add gameplay

--- src/components/Token/Token.vue
+++ src/components/Token/Token.vue
@@ -11,6 +11,7 @@
 
 <script>
 	import VueTypes from 'vue-types';
+
 	export default {
 		name: 'Token',
 		props: {
@@ -33,30 +34,36 @@
 		background-color: $white;
 		cursor: pointer;
 		transition: transform 0.3s linear;
+
 		&:hover,
 		&:focus {
 			outline: none;
 			transform: scale(1.15);
 		}
+
 		@media (max-width: 430px) {
 			width: 80px;
 			height: 80px;
 		}
+
 		&--image {
 			width: 45px;
 			height: 50px;
 			z-index: 2;
+
 			@media (max-width: 430px) {
 				width: 25px;
 				height: 30px;
 			}
 		}
+
 		@each $name, $from, $to in $token-colors {
 			&.#{$name} {
 				background: linear-gradient(to bottom, $from 50%, $to);
 				box-shadow: 0 6px darken($to, 15);
 			}
 		}
+
 		&::after {
 			content: '';
 			position: absolute;
@@ -66,23 +73,29 @@
 			border-radius: 50%;
 			z-index: 1;
 			box-shadow: inset 0px 6px 0px 0px darken($white, 15);
+
 			@media (max-width: 430px) {
 				width: 65px;
 				height: 65px;
 			}
 		}
+
 		&.rock {
 			grid-area: rock;
 		}
+
 		&.paper {
 			grid-area: paper;
 		}
+
 		&.scissors {
 			grid-area: scissors;
 		}
+
 		&.lizard {
 			grid-area: lizard;
 		}
+
 		&.spock {
 			grid-area: spock;
 		}
